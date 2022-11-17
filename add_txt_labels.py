8b80e43246afcba9a6f844b08a43cb613bf438b8
thundrio <12samn@gmail.com>
2022-11-16 17:52:53 -0800

    add labels

--- add_txt_labels.py
+++ add_txt_labels.py
@@ -48,43 +48,43 @@ for root, dirs, files in os.walk(os.path.join("bdscript")):
             if comment:
                 lines[l] = lines[l] + " ; " + comment
 
-        # # check for known references to line numbers
-        # revealed_codes = {}
-        # for l in range(len(lines)):
-        #     line = lines[l]
-        #     if line.startswith("; codeRevealer:"):
-        #         # This can be assumed to only appear once before anything else important
-        #         revealed_codes = [c.strip() for c in line.split("-l")[1:]]
-        #     if "trap_act_table_add" in line:
-        #         for l_r in range(1, 10):
-        #             comment = ""
-        #             line_reverse = lines[l-l_r]
-        #             lr_word = line_reverse.strip().split(" ")
-        #             if lr_word[1] != "0" and not lr_word[1].startswith("L"):
-        #                 if lr_word[1] in revealed_codes:
-        #                     if not "___ref" in line_reverse:
-        #                         lines[l-l_r] = line_reverse.replace(lr_word[1], "L{}".format(lr_word[1]))
-        #                         comment += "___ref"
-        #                 elif "___unk_ref" not in line_reverse:
-        #                     comment += "___unk_ref"
-        #                     print("Warn Unk Ref", fn)
-        #             if comment:
-        #                 lines[l-l_r] += " ; " + comment
+        # check for known references to line numbers
+        revealed_codes = {}
+        for l in range(len(lines)):
+            line = lines[l]
+            if line.startswith("; codeRevealer:"):
+                # This can be assumed to only appear once before anything else important
+                revealed_codes = [c.strip() for c in line.split("-l")[1:]]
+            if "trap_act_table_add" in line:
+                for l_r in range(1, 10):
+                    comment = ""
+                    line_reverse = lines[l-l_r]
+                    lr_word = line_reverse.strip().split(" ")
+                    if lr_word[1] != "0" and not lr_word[1].startswith("L"):
+                        if lr_word[1] in revealed_codes:
+                            if not "___ref" in line_reverse:
+                                lines[l-l_r] = line_reverse.replace(lr_word[1], "L{}".format(lr_word[1]))
+                                comment += "___ref"
+                        elif "___unk_ref" not in line_reverse:
+                            comment += "___unk_ref"
+                            print("Warn Unk Ref", fn)
+                    if comment:
+                        lines[l-l_r] += " ; " + comment
                         
-        #             # These should all be of the form, pushImm word
+                    # These should all be of the form, pushImm word
 
-        #         # Look at last 9 lines, make them L as long as it's found in revealed codes, otherwise a __unk_ref
+                # Look at last 9 lines, make them L as long as it's found in revealed codes, otherwise a __unk_ref
         
-        # # check for possible references to line numbers
-        # for l in range(len(lines)):
-        #     line = lines[l]
-        #     words = line.split(";")[0].split(" ")
-        #     comment = ""
-        #     for word in words:
-        #         if word in revealed_codes and "___possible_ref" not in line:
-        #             comment += "___possible_ref"
-        #     if comment:
-        #         lines[l] = lines[l] + " ; " + comment
+        # check for possible references to line numbers
+        for l in range(len(lines)):
+            line = lines[l]
+            words = line.split(";")[0].split(" ")
+            comment = ""
+            for word in words:
+                if word in revealed_codes and "___possible_ref" not in line:
+                    comment += "___possible_ref"
+            if comment:
+                lines[l] = lines[l] + " ; " + comment
 
         with open(fn, "w") as f:
             f.write("\n".join(lines))
