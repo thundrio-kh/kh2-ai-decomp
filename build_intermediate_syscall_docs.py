998bfb0c3fc91112ec1fd092d00b63e058d87ef2
thundrio <12samn@gmail.com>
2025-04-02 16:45:31 -0700

    example updates

--- build_intermediate_syscall_docs.py
+++ build_intermediate_syscall_docs.py
@@ -68,71 +68,81 @@ for i in range(0, len(argspec_details), 2):
 trap_appears = {}
 trap_examples = {}
 trap_params = {}
-for root, dirs, files in os.walk(os.path.join("bdscript")):
-    for ff in files:
-        fn = os.path.join(root, ff)
+bdscript_parse_order = ["ard", "msn", "root", "obj", "magic", "limit", "gumimenu"]
+for bsf in bdscript_parse_order:
+    for root, dirs, files in os.walk(os.path.join("bdscript",bsf)):
+        for ff in files:
+            fn = os.path.join(root, ff)
 
-        text = open(fn).read()
+            text = open(fn).read()
 
-        for tname in trap_argspecs:
+            for tname in trap_argspecs:
 
-            # JUST BUILD THIS
-            # re.findall(r'\n.*?\n.*?\n.*?trap_vector_mul.*?\n.*?\n', text)
+                # JUST BUILD THIS
+                # re.findall(r'\n.*?\n.*?\n.*?trap_vector_mul.*?\n.*?\n', text)
 
-            # syscall_search = re.findall(r'\((\d).*?(\d)', trap_argspecs[tname]) # syscall 0, 2 ; trap_puts (1 in, 0 out)
-            # nin = int(syscall_search[0][0])
-            # nout = int(syscall_search[0][1])
+                # syscall_search = re.findall(r'\((\d).*?(\d)', trap_argspecs[tname]) # syscall 0, 2 ; trap_puts (1 in, 0 out)
+                # nin = int(syscall_search[0][0])
+                # nout = int(syscall_search[0][1])
 
-            # re_string = r'\n'
-            # for _ in range(nin):
-            #     re_string += '.*\n'
-            # re_string += tname
-            # re_string += ".*\n"
-            # for _ in range(nout):
-            #     re_string += '.*\n'
+                # re_string = r'\n'
+                # for _ in range(nin):
+                #     re_string += '.*\n'
+                # re_string += tname
+                # re_string += ".*\n"
+                # for _ in range(nout):
+                #     re_string += '.*\n'
 
-# push unk1 ; (unknown) 
-# push unk2 ; (unknown) 
-# syscall 0, 36 ; trap_vector_mul (2 in, 1 out)
-# pop unk ; (unknown) 
+    # push unk1 ; (unknown) 
+    # push unk2 ; (unknown) 
+    # syscall 0, 36 ; trap_vector_mul (2 in, 1 out)
+    # pop unk ; (unknown) 
 
-            if tname+" " in text:
-                syscall_params = re.findall(r'\((\d*).*?(\d)', trap_argspecs[tname]) # syscall 0, 2 ; trap_puts (1 in, 0 out)
-                nin = int(syscall_params[0][0])
+                if tname+" " in text:
+                    syscall_params = re.findall(r'\((\d*).*?(\d)', trap_argspecs[tname]) # syscall 0, 2 ; trap_puts (1 in, 0 out)
+                    nin = int(syscall_params[0][0])
 
-                param_re = r'\n(' + '\n'.join(['.*?' for _ in range(nin)]) + '\n).*?' + tname + ' '
+                    param_re = r'\n(' + '\n'.join(['.*?' for _ in range(nin)]) + '\n).*?' + tname + ' '
 
-                if tname not in trap_params:
-                    trap_params[tname] = [{} for _ in range(nin)]
+                    if tname not in trap_params:
+                        trap_params[tname] = [{} for _ in range(nin)]
 
-                if tname not in trap_appears:
-                    trap_appears[tname] = []
-                if "00common" in fn:
-                    endname = "00common"
-                else:
-                    endname = fn[9:]
-                trap_appears[tname].append(endname)
+                    if tname not in trap_appears:
+                        trap_appears[tname] = []
+                    if "00common" in fn:
+                        endname = "00common"
+                    else:
+                        endname = fn[9:]
+                    trap_appears[tname].append(endname)
 
-                if nin > 0:
-                    param_finds = re.findall(param_re, text)
-                    for find in param_finds:
-                        params = find.strip().split("\n")
-                        for i in range(len(params)):
-                            param = params[i].strip()
-                            param_words = param.split(" ")
-                            inst = param_words[0]
-                            values = " ".join(param_words[1:])
-                            if inst not in trap_params[tname][i]:
-                                trap_params[tname][i][inst] = set()
-                            trap_params[tname][i][inst].add(values)
+                    if nin > 0:
+                        param_finds = re.findall(param_re, text)
+                        for find in param_finds:
+                            params = find.strip().split("\n")
+                            for i in range(len(params)):
+                                param = params[i].strip()
+                                param_words = param.split(" ")
+                                inst = param_words[0]
+                                values = " ".join(param_words[1:])
+                                if inst not in trap_params[tname][i]:
+                                    trap_params[tname][i][inst] = set()
+                                trap_params[tname][i][inst].add(values)
 
-                if not tname in trap_examples:
-                    # This regex is broken so just do it without regex
-                    # I think the regex should just be any thing for the label name
+                    if not tname in trap_examples:
+                        trap_examples[tname] = {"result": None, "backup_result": None}
+
                     regex = r'([\w]+:[^:]*?' + tname +' .*?)(?=\n[\w]+:|\Z)'
                     regex_labels = re.findall(r'([TL]R?\d*:[^:]*?'+tname+' [^:]*?)\n.*?:', text)
-                    trap_examples[tname] = (endname, regex_labels[0])
-
+                    if not trap_examples[tname]["backup_result"]:
+                        trap_examples[tname]["backup_result"] = (endname, regex_labels[0])
+                    elif not trap_examples[tname]["result"]:
+                        min_length = nin+1
+                        max_length = 15 # just an initial guess
+                        for lb in sorted(regex_labels, key=lambda s: s.count('\n')):
+                            nlines = lb.count('\n')
+                            if nlines >= min_length and nlines <= max_length:
+                                trap_examples[tname]["result"] = (endname, lb)
+                    
 objects = {
     obj.split("\t")[1]: obj.split("\t")[2]
     for obj in open("objlist.tsv").read().split("\n")
@@ -177,8 +187,10 @@ for syscall in syscall_list:
     decompiled_code = trap_functions.get(name, "Not Found")
 
     appears_in = ["{} ({})".format(n, objects.get(n.split(os.sep)[-2], "")) if n.startswith("obj") else n for n in trap_appears.get(name, [])]
-    example_name = trap_examples.get(name, ["NA"])[0]
-    examplecode = trap_examples.get(name, [""])[-1]
+    example_options_for_trap = trap_examples.get(name, {"result": ("NA","")})
+    found_example = example_options_for_trap["result"] if example_options_for_trap["result"] else example_options_for_trap["backup_result"]
+    example_name = found_example[0]
+    examplecode = found_example[1]
 
 
    
